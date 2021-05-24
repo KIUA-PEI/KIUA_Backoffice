067cf09d3dcbf46cfcc4fe9a64b6ee817f81a0ad
JPCGameiro <joao.gameiro@ua.pt>
2021-05-25 00:12:55 +0100

    Página de dashboards

--- app.py
+++ app.py
@@ -14,3 +14,10 @@ def register():
 @app.route('/home')
 def home():
     return render_template("home.html")
+
+@app.route('/dashboards')
+def dashboards():
+    return render_template("dashboards.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)

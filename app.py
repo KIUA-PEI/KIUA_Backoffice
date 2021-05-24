1621c2a29be1cd02c9254fb50c413c75c36ab32a
Pedro Abreu <pedro.abreu@ua.pt>
2021-05-24 19:03:11 +0100

    add template

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
+
 @app.route('/')
-def hello_world():
-    return 'Hello world!'
+def login():
+    return render_template("index.html")
+
+@app.route('/register')
+def register():
+    return render_template("register.html")
+
+@app.route('/home')
+def home():
+    return render_template("home.html")

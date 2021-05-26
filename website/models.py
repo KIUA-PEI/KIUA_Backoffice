6c9ef94bce6f1662406658b56519fee252aa3c61
Pedro Abreu <pedro.abreu@ua.pt>
2021-05-26 16:53:30 +0100

    db

--- website/models.py
+++ website/models.py
@@ -1,6 +1,16 @@
 
+from datetime import timezone
+from enum import unique
+
+from sqlalchemy.orm import backref
+from sqlalchemy.sql.sqltypes import ARRAY
+from website.views import dashboards
 from website import db
 from flask_login import UserMixin
+from sqlalchemy.sql import func
+from sqlalchemy.ext.mutable import MutableList
+from sqlalchemy import PickleType
+
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
@@ -8,3 +18,92 @@ class User(db.Model, UserMixin):
     password = db.Column(db.String(150))
     fname = db.Column(db.String(150))
     lname = db.Column(db.String(150))
+    # dashboards do utilizador
+    dashboards = db.relationship("Dashboard", backref='user', lazy=True)
+    # metricas do utilizador(dividas pelo tipo de api)
+    basic = db.relationship("Basic_url", backref="user", lazy=True)
+    key = db.relationship("Key_url", backref="user", lazy=True)
+    http = db.relationship("Http_url", backref="user", lazy=True)
+    token = db.relationship("Token_url", backref="user", lazy=True)
+
+class Dashboard(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    uid = db.Column(db.Integer, unique=True)
+    # data criação
+    date = db.Column(db.DateTime(timezone=True), default=func.now())
+    visibilidade = db.Column(db.Integer, nullable=False) # 0 -> privada, 1 -> pública
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+    panels = db.relationship("Panels", backref="dashboard", lazy=True)
+
+
+# cada gráfico de uma dashboard
+class Panels(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    kpi = db.Column(MutableList.as_mutable(PickleType)) # lista com o nome dos kpi que o utilizador selecionou na checkbox
+    dashboard_id = db.Column(db.Integer, db.ForeignKey("dashboard.id"), nullable=False)
+
+# -----
+
+class Basic_url(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    url = db.Column(db.String(500),nullable=False)
+    # data criação
+    date = db.Column(db.DateTime(timezone=True), default=func.now())
+    args = db.Column(db.String(750),nullable=True)
+    period = db.Column(db.Integer(), nullable=False)
+    status = db.Column(db.Boolean(),default=True)
+    error = db.Column(db.String(75))
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+
+    def __repr__(self):
+        return f"API(URL = {self.url}, period={self.period}, status = {self.status})"
+      
+class Key_url(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    url = db.Column(db.String(500),nullable=False)
+    # data criação
+    date = db.Column(db.DateTime(timezone=True), default=func.now())
+    args = db.Column(db.String(750),nullable=True)
+    period = db.Column(db.Integer())
+    key = db.Column(db.String(300))
+    status = db.Column(db.Boolean(),nullable=False,default=True)
+    error = db.Column(db.String(75))
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+
+    def __repr__(self):
+        return f"API(URL={self.url}, period={self.period},status={self.status})"
+    
+class Http_url(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    url = db.Column(db.String(500),nullable=False)
+    # data criação
+    date = db.Column(db.DateTime(timezone=True), default=func.now())
+    args = db.Column(db.String(750),nullable=True)
+    period = db.Column(db.Integer())
+    key = db.Column(db.String(300))
+    username = db.Column(db.String(300))
+    status = db.Column(db.Boolean(),nullable=False,default=True)
+    error = db.Column(db.String(75))
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+
+    def __repr__(self):
+        return f"API(URL={self.url}, period={self.period},status={self.status})"  
+    
+class Token_url(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    url = db.Column(db.String(500),nullable=False)
+    # data criação
+    date = db.Column(db.DateTime(timezone=True), default=func.now())
+    token_url = db.Column(db.String(500),nullable=False)
+    args = db.Column(db.String(750),nullable=True)
+    period = db.Column(db.Integer())
+    key = db.Column(db.String(300))
+    secret = db.Column(db.String(300))
+    content_type = db.Column(db.String(50))
+    auth_type = db.Column(db.String(50))
+    status = db.Column(db.Boolean(),nullable=False,default=True)
+    error = db.Column(db.String(75))
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+
+    def __repr__(self):
+        return f"API(URL={self.url}, period={self.period}, status={self.status})"
--- website/views.py
+++ website/views.py
@@ -13,3 +13,19 @@ def home():
 @login_required
 def dashboards():
     return render_template("dashboards.html")
+
+@views.route("/mymetrics")
+@login_required
+def mymetrics():
+    return render_template("mymetrics.html")
+
+@views.route("/metrics")
+@login_required
+def metrics():
+    return render_template("metrics.html")
+
+@views.route("/help")
+@login_required
+def help():
+    return render_template("help.html")
+    

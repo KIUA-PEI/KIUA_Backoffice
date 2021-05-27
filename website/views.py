3680e0c3804dc3864444b7367d03148767f8babd
JPCGameiro <joao.gameiro@ua.pt>
2021-05-27 17:22:27 +0100

    página de  escolha de métricas

--- website/views.py
+++ website/views.py
@@ -1,14 +1,29 @@
 from flask import Blueprint
 from flask.templating import render_template
 from flask_login import login_required, current_user
+from flask import request, flash
 
 views = Blueprint("views", __name__)
 
 
-@views.route("/dashboards")
+@views.route("/dashboards", methods=["GET", "POST"])
 @login_required
 def dashboards():
+    if request.method == "POST":
+        d = request.form.get("d")
+
+        print("Dash"+d)
+        if d == "":
+            flash('Invalid Dashboard name', category='error')
+            print("Here")
+        else:
+            return render_template("createdashboards.html", dash = d)
     return render_template("dashboards.html")
+
+@views.route("/createdashboard")
+@login_required
+def createdashboards():
+    return render_template("createdashboards.html")
 
 @views.route("/mymetrics")
 @login_required

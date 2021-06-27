51879617c105c211e20fb82fea99592cab91299e
JPCGameiro <joao.gameiro@ua.pt>
2021-06-27 23:18:21 +0100

    Ready to deploy

---
+++ website/config.py
@@ -0,0 +1,7 @@
+server = "http://40.68.96.164:3000"
+
+api_key = "eyJrIjoiT0tPNkEwZlBQR1N4WHYyTFhGMEh6bDBpZXpWRmhUMWoiLCJuIjoicGVpa2V5IiwiaWQiOjF9"
+
+daemons_api = "http://51.103.48.159"
+
+daemons_api_key = 'ASDzxcdwekjkads786zxc123asdzxc98788ASd9231sz76238'
